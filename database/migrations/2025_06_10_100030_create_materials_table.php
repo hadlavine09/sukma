f8c8f941d68985c387a8acf631a3c34bfb889431
hadlavine09 <haddadhikmah00@gmail.com>
2025-06-11 08:03:56 +0700

    update bug migration

--- database/migrations/2025_06_10_100030_create_materials_table.php
+++ database/migrations/2025_06_10_100030_create_materials_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->string('kode_material');
             $table->string('nama_material');
             $table->string('jumlah_material');
-            $table->string('jumlah_material');
             $table->timestamps();
         });
     }
--- resources/views/backend/component/sidebar.blade.php
+++ resources/views/backend/component/sidebar.blade.php
@@ -24,6 +24,13 @@
         <li><a class="treeview-item" href="{{ route('produk.index')}}"><i class="icon bi bi-circle-fill"></i> Produk</a></li>
       </ul>
     </li>
+    <li class="treeview"><a class="app-menu__item" href="{{ asset('assets_backend/#')}}" data-toggle="treeview"><i class="app-menu__icon bi bi-ui-checks"></i><span class="app-menu__label">Manajemen Transaksi</span><i class="treeview-indicator bi bi-chevron-right"></i></a>
+      <ul class="treeview-menu">
+        <li><a class="treeview-item" href="{{ route('tag.index')}}"><i class="icon bi bi-circle-fill"></i> Keranjang</a></li>
+        <li><a class="treeview-item" href="{{ route('kategori.index')}}"><i class="icon bi bi-circle-fill"></i> Checkout</a></li>
+        <li><a class="treeview-item" href="{{ route('produk.index')}}"><i class="icon bi bi-circle-fill"></i> Transaksi</a></li>
+      </ul>
+    </li>
     <li class="treeview"><a class="app-menu__item" href="{{ asset('assets_backend/#')}}" data-toggle="treeview"><i class="app-menu__icon bi bi-ui-checks"></i><span class="app-menu__label">Manajemen Material</span><i class="treeview-indicator bi bi-chevron-right"></i></a>
       <ul class="treeview-menu">
         <li><a class="treeview-item" href="{{ route('tag.index')}}"><i class="icon bi bi-circle-fill"></i> Material</a></li>
